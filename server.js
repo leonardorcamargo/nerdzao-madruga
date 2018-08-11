c02103cc9c19023dc039379482bfac0fd8f02628
Leonardo R. Camargo <leonardorcamargo@gmail.com>
2018-08-11 02:53:58 -0300

    Ajustes na api para conectar em banco de dados mongodb. Inicio do projeto em vue

--- server.js
+++ server.js
@@ -1,16 +1,37 @@
 const express = require('express');
+const mongo = require('mongodb');
 
 async function main() {
 	const app = express();
-	app.get('/', (req, res) => {
-		res.send('GET /personagens');
-	});
+	try{
+		// const connection = await mongo.connect(
+		// 	'mongodb://ronaldinho:nerdzao1@ds213832.mlab.com:13832/'
+		// );
+		// const db = connection.db('nerdzao-rick-and-morty');
+		// const characters = db.collection('characteres');
+		const personagens = [
+			{
+				nome: "Rick Sanchez",
+				dimension: "C-137",
+			},
+			{
+				nome: "Morty Smith",
+				dimension: "C-137",
+			}
+		];
 
-	app.get('/personagens', (req, res) => {
-		res.send('oi');
-	});
+		app.get('/', (req, res) => {
+			res.send('GET /personagens');
+		});
+		app.get('/personagens', async (req, res) => {
+			res.send(personagens);
+		});
+		app.listen(3000, () => console.log('Escutando na porta 3000'));
+	} catch(e) {
+		console.log(e);
+	}
 
-	app.listen(3000, () => console.log('Escutando na porta 3000'));
+	
 }
 
 main();
